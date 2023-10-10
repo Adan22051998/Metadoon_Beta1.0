d9afa832ae6607f7ed28ac7ecc05823782e710dc
Adan22051998 <adanrodrigues1998@gmail.com>
2023-10-09 22:56:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter import scrolledtext
 from tkinter.tix import *
+from tkinter import messagebox
 
 metainfo = Tk()
 metainfo.title("Metadoon")
@@ -41,7 +42,119 @@ metainfo.configure(background="blue",)
 #icon app
 metainfo.iconbitmap(default='./Metadoon.ico')
 #Defining Functions
+#class container:
+class container:
+    dir_name = os.path.dirname(__file__)
+    _LIST_db = ["Use your own database..."]
 
+
+#class to functions that shall modify the window
+class _functions_:
+    class window_functions:
+        def create_file():
+            os.system(f'')
+        def openf():
+            try:
+                file_dir = filedialog.askopenfilename()
+                return file_dir
+            except:
+                messagebox.showinfo("METADDOON","This is not a valid directory!")
+        def executar_comando():
+            try:
+                name_usr = getpass.getuser()
+                n_sistem = platform.system()
+                comand_ = entry_comand.get()
+                output = "Prompt " + name_usr + n_sistem + comand_
+                return texto_output.insert(END, output + "\n")
+            except:
+                None
+#classes of aplications
+class FastQC:
+    outputqc =''
+    inputqc = ''
+    def do_fatqc():
+        try:
+            os.system(fr'wsl fastqc -o {outputqc} {inputqc}')
+        except:
+            print('Something went wrong!')
+            messagebox.showinfo("METADDOON","Something Went Wrong")
+class Trimmomatic:
+    threads = ''
+    trimput_1 = ''
+    trimput_2 = ''
+    trimpout_1 = ''
+    trimpout_2 = ''
+    trimming_options = ''
+    def trim():
+        try:
+            os.system(fr'trimmomatic PE [-threads <n>] <input_1.fq> <input_2.fq> <output_1.fq> <output_2.fq> <options>')
+        except:
+            print('Something went wrong!')
+            messagebox.showinfo("METADDOON","Something Went Wrong")
+class usearch:
+    def merge():
+        try:
+            n1 = en_miss.get()
+            n2 = en_pctid.get()
+            os.system(fr'./_Usearch -fastq_mergepairs ./Sample/*_R1*.fastq -fastq_maxdiffs {n1} -fastq_pctid {n2} -fastqout ./Metarquivos/Mesclados/_merged.fq -relabel @')
+            messagebox.showinfo("METADDOON:", "Fastq/Fasta files merged sucessfuly...")
+            return print("Samples Mergeds Sucessfully.")
+        
+        except:
+            messagebox.showinfo("METADDOON:", "Something went wrong!")
+            return print("ERROR")
+    def quali():
+        try:
+            n1 = en_MEE.get()
+            os.system(fr'./_Usearch -fastq_filter ./Metarquivos/Mesclados/_merged.fq -fastq_maxee {n1} -fastaout ./Metarquivos/Filtro_de_qualidade/_filtered.fa')
+            messagebox.showinfo("METADDOON:", "Files filtrated.")
+            return print("Quality Filter Applyed.")
+        except:
+            messagebox.showinfo("METADDOON:", "Something went wrong!")
+            return print("ERROR")
+    def uniq():
+        try:
+            os.system(fr'./_Usearch -fastx_uniques ./Metarquivos/Filtro_de_qualidade/_filtered.fa -fastaout ./Metarquivos/Leituras_unicas/_uniques.fa -relabel uniq -sizeout')
+            messagebox.showinfo("METADDOON:", "Unique sequences successfully stored.")
+        except:
+            messagebox.showinfo("METADDOON:", "Something went wrong!")
+            return print("ERROR")
+
+    def otus():
+        try:
+            os.system(fr'./_Usearch -cluster_otus ./Metarquivos/Leituras_unicas/_uniques.fa -minsize 2 -otus ./Metarquivos/Geracao_de_otus/_otus.fa -relabel otu')
+            messagebox.showinfo("METADDOON:", "OTUS verification done successfully.")
+            return print("The OTU's Have Been Clustered Sucessfully")
+        except:
+            messagebox.showinfo("METADDOON:", "Something went wrong!")
+            return print("ERROR")
+    def otutable():
+        try:
+            os.system(fr'./_Usearch -usearch_global ./Metarquivos/Mesclados/_merged.fq  -db ./Metarquivos/Geracao_de_otus/_otus.fa -strand plus -id 0.97 -otutabout ./Metarquivos/Tabela_de_OTU/_otutable.txt')
+            os.system("cd ./Metarquivos/Tabela_de_OTU/ | cp ./Metarquivos/Tabela_de_OTU/_otutable.txt ./Metarquivos/Tabela_de_OTU/_otutable.tsv")
+            os.system(fr"Rscript {container.dir_name}/CHAO1.R")
+            os.system(fr"Rscript {container.dir_name}/SHANNON.R")
+            os.system(fr"Rscript {container.dir_name}/Ace.R")
+            os.system(fr"Rscript {container.dir_name}/SIMPSON.R")
+            os.system(fr"Rscript {container.dir_name}/RIQUEZA.R")
+            os.system(fr"Rscript {container.dir_name}/INV_SIMPSON.R")
+            messagebox.showinfo("METADDOON:", "The Otutable is Ready now...")
+            return print("")
+        except:
+            messagebox.showinfo("METADDOON:", "Something went wrong!")
+            return print("ERROR")
+    def otu16s():
+        try:
+            xvis_db = CB_DB.get()
+            if xvis_db == container._LIST_db[0]:
+                db_file_path = filedialog.askopenfilename()
+                os.system(fr'./_Usearch -nbc_tax ./Metarquivos/Geracao_de_otus/_otus.fa -db {db_file_path} -strand plus -tabbedout ./Metarquivos/XVI_S_DB/_taxonomy.tsv')
+                os.system(fr"Rscript {container.dir_name}/taxax.R")
+            messagebox.showinfo("METADDOON:", "Base de dados verificada...")
+            return print("DB verification Done.")
+        except:
+            messagebox.showinfo("METADDOON:", "Something went wrong!")
+            return print("ERROR")
 #-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_#
 #Creating-Layouts
 f1 = Canvas(metainfo,background='#04322b')
